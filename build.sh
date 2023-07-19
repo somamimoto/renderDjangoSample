4ce74340dcd3351339b3c8b1c43168476bd272fc
somamimoto <happyvetlife@gmail.com>
2023-07-19 16:46:26 +0900

    static settings

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -o errexit
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-# python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 python manage.py migrate
