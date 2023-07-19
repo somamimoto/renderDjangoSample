8e59f8fce9f33592880c1a81d3f1dca8e93358c0
somamimoto <happyvetlife@gmail.com>
2023-07-19 16:25:14 +0900

    modified

--- renderDjango_main/settings.py
+++ renderDjango_main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['*.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['render-django-sample.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
